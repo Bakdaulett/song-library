85697d0720f179e0eef258b5632a8a8a65c08d3d
Bakdaulet <bakdauletsambilov@gmail.com>
2024-11-23 15:13:25 +0500

    Some changes

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,23 @@
 package logger
+
+import (
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+func InitLogger(level string) *logrus.Logger {
+	log := logrus.New()
+	logLvl, err := logrus.ParseLevel(level)
+	if err != nil {
+		logLvl = logrus.InfoLevel
+	}
+
+	log.SetLevel(logLvl)
+	log.SetOutput(os.Stdout)
+
+	log.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+
+	return log
+}
